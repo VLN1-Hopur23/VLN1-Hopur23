4fbbf95caab8293e9aa0fdfd8a1730f96c36df4c
Einar Þórir Árnason <einsiarna@gmail.com>
2016-12-05 09:49:32 +0000

    Version1 moved to folder

---
+++ version1/consoleui.cpp
@@ -0,0 +1,308 @@
+#include "consoleui.h"
+
+using namespace std;
+
+// Presentation layer
+
+ConsoleUI::ConsoleUI()
+{
+}
+
+void ConsoleUI::run()
+{
+    string command;
+    bool loop = true;
+
+    bool openFileWorks= _service.load();
+
+    if (openFileWorks)
+    {
+        while(loop == true)
+        {
+            cout << endl;
+            cout << "Choose a command:\n";
+            cout << endl;
+            cout << "register\t- Register a known character from computer science\n";
+            cout << "list\t\t- Display the list of computer scientists\n";
+            cout << "search\t\t- Search\n";
+            cout << "edit\t\t- Edit computer scientist\n";
+            cout << "delete\t\t- Delete computer scientist\n";
+            cout << "quit\t\t- Exit program\n";
+            cout << endl;
+
+            cin >> command;
+            cout << endl;
+
+            // Frequent users could use one lower case letter for shortcut
+            if (command == "list" || command == "List" || command == "l")
+            {
+                List();
+            }
+            else if (command == "register" || command == "Register" || command == "r")
+            {
+                Register();
+            }
+            else if (command == "search" || command == "Search" || command == "s")
+            {
+                Search();
+            }
+            else if (command == "edit" || command == "Edit" || command == "e")
+            {
+                Edit();
+            }
+            else if (command == "delete" || command == "Delete" || command == "d")
+            {
+                Delete();
+            }
+            else if (command == "quit" || command == "Quit" || command == "q")
+            {
+                loop = false;
+            }
+            else
+            {
+                cout << "Please choose one of the given options!\n";
+            }
+        }
+    }
+    else
+    {
+        cout << "Error with opening file" << endl;
+    }
+}
+
+void ConsoleUI::Edit()
+{
+    cout << "Edit registered computer scientist character" << endl;
+    cout << endl;
+
+    int index;
+    string change, input;
+
+    cout << "Enter the index number of the computer scientist you want to change: ";
+    cin >> index;
+    while (cin.fail() || index > _service.getSize() || index < 0)
+    {
+        cout << "ERROR!! Please enter a valid index!\n";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> index;
+    }
+    cout << "Enter what it is that you want to change about the computer scientist" << endl;
+    cout << "(name/gender/birth/death): ";
+    cin >> change;
+    while (change != "name" && change != "gender" && change != "birth" && change != "death"){
+        cout << "ERROR!! Please enter a valid option! (name/gender/birth/death)\n";
+        cin.clear();
+        cin >> change;
+    }
+    cout << "Enter the new value: ";
+    cin >> input;
+
+    string message = _service.editScientist(index, change, input);
+
+    cout << endl;
+    cout << message << endl;
+    cout << endl;
+}
+
+void ConsoleUI::Delete()
+{
+    int index;
+    char confirm;
+
+    cout << "Delete registered scientist" << endl;
+    cout << endl;
+    cout << "Enter the index number of the scientist to delete: ";
+    cin >> index;
+    while (cin.fail() || index > _service.getSize() || index < 0)
+    {
+        cout << "ERROR!! Please enter a valid index!\n";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> index;
+    }
+
+    cout << "Are you sure you want to delete " << _service.getScientist(index).getName() << "? (y/n): ";
+    cin >> confirm;
+
+    if (confirm == 'y' || confirm == 'Y')
+    {
+        _service.deleteScientist(index);
+
+        cout << endl;
+        cout << "Successfully deleted!" << endl;
+        cout << endl;
+    }
+}
+
+void ConsoleUI::Search()
+{
+    string searchData;
+
+    cout << "Searching by first letter/s in the name" << endl;
+    cout << "Please write the letter/s of the scientist/s that you want to display" << endl;
+    cin >> searchData;
+
+    vector<int> foundScientists = _service.searchScientists(searchData);
+
+    cout << endl;
+
+    printFrame();
+    printHeader();
+
+    for (size_t i = 0; i < foundScientists.size(); i++)
+    {
+        cout.fill('0');
+        cout.width(2);
+        cout << right << foundScientists[i] << ". ";
+        cout.fill(' ');
+        cout.width(25);
+        cout << left;
+        cout << _service.getScientist(foundScientists[i]).getName();
+        cout << "\t";
+        cout << _service.getScientist(foundScientists[i]).getGender();
+        cout << "\t";
+        cout << left << _service.getScientist(foundScientists[i]).getYearOfBirth();
+        cout << "\t";
+        cout << left << _service.getScientist(foundScientists[i]).getYearOfDeath();
+        cout << "\t";
+        cout << left << _service.getScientist(foundScientists[i]).getAge();
+        cout << endl;
+    }
+
+    printFrame();
+
+    cout << endl;
+}
+
+void ConsoleUI::List()
+{
+    string sort;
+
+    while(sort != "return" && sort!= "Return" && sort != "r")
+    {
+        cout << endl;
+        cout << "Write the option how you want your list sorted\n";
+        cout << endl;
+        cout << "name\t\t- Sort by name\n";
+        cout << "age\t\t- Sort by age\n";
+        cout << "birth\t\t- Sort by year of birth\n";
+        cout << "death\t\t- Sort by year of death\n";
+        cout << "return\t\t- Return to main menu\n";
+        cout << endl;
+
+        cin >> sort;
+
+        vector<Scientist> _AllScientist = _service.getScientistVector();
+        _AllScientist = SortVector( _AllScientist, sort);
+
+        displayListOfScientist();
+    }
+}
+
+void ConsoleUI::Register()
+{
+    string name;
+    string gender;
+    int yearOfBirth;
+    int yearOfDeath;
+
+    cout << "Enter the name of the person:" << endl;
+    cin.ignore();
+    getline(cin,name);
+
+    cout << "Enter gender (m for male, f for female):" << endl;
+    cin >> gender;
+
+    while (gender != "m" && gender != "M" && gender != "f" && gender != "F")
+    {
+        cout << "Please enter either m or f!\n";
+        cin.clear();
+        cin >> gender;
+    }
+
+    cout << "Enter year of birth:" << endl;
+    cin >> yearOfBirth;
+    while (cin.fail())
+    {
+        cout << "ERROR!! Please enter a valid option!\n";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> yearOfBirth;
+    }
+    while (yearOfBirth > _time.getYearToDay() || yearOfBirth < 0)
+    {
+        cout << "ERROR!! Please enter a valid year of birth!\n";
+        cin.clear();
+        cin >> yearOfBirth;
+    }
+
+    cout << "Enter year of death or 0 if the person is still alive:" << endl;
+    cin >> yearOfDeath;
+    while (cin.fail())
+    {
+        cout << "ERROR!! Please enter a valid option!\n";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> yearOfDeath;
+    }
+    while (yearOfDeath > _time.getYearToDay() || ((yearOfDeath < yearOfBirth) && yearOfDeath != 0) )
+    {
+        cout << "ERROR!! Please enter a valid year of death!\n";
+        cin.clear();
+        cin >> yearOfDeath;
+    }
+
+    Scientist scientist(name, gender, yearOfBirth, yearOfDeath);
+    _service.addScientist(scientist);
+
+    cout << "Scientist added!" << endl;
+    cout << endl;
+}
+
+void ConsoleUI::displayListOfScientist()
+{
+    printFrame();
+    printHeader();
+
+    for (size_t i = 0; i < _service.getSize(); i++)
+    {
+        cout.fill('0');
+        cout.width(2);
+        cout << right << i << ". ";
+        cout.fill(' ');
+        cout.width(25);
+        cout << left;
+        cout << _service.getScientist(i).getName();
+        cout << "\t";
+        cout << _service.getScientist(i).getGender();
+        cout << "\t";
+        cout << left << _service.getScientist(i).getYearOfBirth();
+        cout << "\t";
+        cout << left << _service.getScientist(i).getYearOfDeath();
+        cout << "\t";
+        cout << left << _service.getScientist(i).getAge();
+        cout << endl;
+    }
+    printFrame();
+}
+
+void ConsoleUI::printFrame()
+{
+    cout.fill('=');
+    cout.width(63);
+    cout << left << "=";
+    cout << endl;
+    cout.fill(' ');
+}
+
+void ConsoleUI::printHeader()
+{
+    cout << "Nr. Scientist name\t\tGender\tBirth\tDeath\tAge" << endl;
+    cout <<"\t\t\t\t\tYear\tYear" << endl;
+}
+
+vector<Scientist> ConsoleUI::SortVector(vector<Scientist> _listOfScientist,string sort)
+{
+   return _service.sortScientists(_listOfScientist, sort);
+}
---
+++ version1/scientistservice.cpp
@@ -0,0 +1,172 @@
+#include "scientistservice.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+ScientistService::ScientistService()
+{
+}
+
+//operator instrument that compares names in alphabetical order
+struct ScientistComparisonNameForward
+{
+    bool operator() (Scientist i, Scientist j) {return (i.getName()<j.getName());}
+};
+
+//operator instrument that compares year of birth in ascending order
+bool ScientistComparisonYearOfBirthForward (Scientist i, Scientist j)
+{
+    return (i.getYearOfBirth()<j.getYearOfBirth());
+}
+
+bool ScientistComparisonYearOfDeathForward (Scientist i, Scientist j)
+{
+    return (i.getYearOfDeath()<j.getYearOfDeath());
+}
+
+bool ScientistComparisonAgeForward (Scientist i, Scientist j)
+{
+    return (i.getAge()<j.getAge());
+}
+
+//sorts scientist by aplhabetical order, by year of birth, by year of death and by age
+ vector<Scientist> ScientistService::sortScientists(vector<Scientist> _listOfScientists, string sort)
+{
+    if (sort == "name" || sort == "Name" || sort == "n")
+
+    {
+        ScientistComparisonNameForward cmp;
+        std::sort(_scientists.begin(), _scientists.end(), cmp);
+    }
+    else if (sort == "birth" || sort == "Birth" || sort == "b")
+    {
+        std::sort(_scientists.begin(), _scientists.end(), ScientistComparisonYearOfBirthForward);
+    }
+    else if (sort == "death" || sort == "Death" || sort == "d")
+    {
+        std::sort(_scientists.begin(), _scientists.end(), ScientistComparisonYearOfDeathForward);
+    }
+    else if (sort == "age" || sort == "Age" || sort == "a")
+    {
+        std::sort(_scientists.begin(), _scientists.end(), ScientistComparisonAgeForward);
+    }
+    else
+    {
+        //default is to sort by name
+        ScientistComparisonNameForward cmp;
+        std::sort(_scientists.begin(), _scientists.end(), cmp);
+    }
+    return _listOfScientists;
+}
+
+vector<int> ScientistService::searchScientists(string& searchData)
+{
+    vector<int> foundScientists;
+
+    transform(searchData.begin(), searchData.end(), searchData.begin(), ::tolower);
+
+    for(unsigned long i = 0; i < _scientists.size(); i++)
+    {
+        string name = _scientists[i].getName();
+        transform(name.begin(), name.end(), name.begin(), ::tolower);
+        if(name.substr(0,searchData.size()) == searchData)
+        {
+            //If we find the scientist then we...
+
+            foundScientists.push_back(i);
+        }
+    }
+    return foundScientists; //remeber to change
+}
+
+void ScientistService::addScientist(Scientist scientist)
+{
+    bool isin = ifExist(scientist.getName());
+
+    if (isin == false)
+    {
+         _scientists.push_back(scientist);
+         _data.writeNewScientist(scientist);
+    }
+}
+
+Scientist ScientistService::getScientist(int index) const
+{
+    return _scientists[index];
+}
+
+int ScientistService::getSize() const
+{
+    return _scientists.size();
+}
+
+bool ScientistService::ifExist(string name)
+{
+   for (size_t i = 0; i < _scientists.size(); i++)
+   {
+       if (_scientists[i].getName() == name)
+       {
+           return true;
+       }
+   }
+   return false;
+}
+
+vector<Scientist> ScientistService::getScientistVector()
+{
+    return _scientists;
+}
+
+bool ScientistService::load()
+{
+    _data.getData(_scientists);
+
+    return DataAccessWorks();
+}
+
+bool ScientistService::DataAccessWorks()
+{
+    if (_data.DataOk && _data.FileOpen)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+string ScientistService::editScientist(int index, string change, string input)
+{
+    string message = "Invalid, ignored";
+    if(index > _scientists.size()){
+        message = "Index out of range";
+    }
+    if (change == "name")
+    {
+        message = "Name changed successfully";
+        _scientists[index].setName(input); 
+    }
+    else if (change == "gender")
+    {
+        message = "Gender changed successfully";
+        _scientists[index].setGender(input);
+    }
+    else if (change == "birth")
+    {
+        message = "Birth year changed successfully";
+        _scientists[index].setYearOfBirth(stoi(input, 0));
+    }
+    else if (change == "death")
+    {
+        message = "Death year changed successfully";
+        _scientists[index].setYearOfDeath(stoi(input, 0));
+    }
+    _data.writeData(_scientists);
+    return message;
+}
+
+void ScientistService::deleteScientist(int index)
+{
+    _scientists.erase(_scientists.begin()+index);
+    _data.writeData(_scientists);
+}
