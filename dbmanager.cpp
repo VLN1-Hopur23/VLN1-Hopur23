fb3de3927101f17bc7867ede9c4be02c49220e10
Gunnþóra Mist <Mist@Bjorns-MacBook-Air-3.local>
2016-12-07 14:53:39 +0000

    search functions added

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -1,12 +1,13 @@
 #include "dbmanager.h"
 
 using namespace std;
-
+//constructor connects to sqlite database
 DbManager::DbManager()
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("VLN1-Hopur23.sqlite");
 
+    //validates the connection
     if (!db.open())
     {
         qDebug() << "Error: connection with database fail";
@@ -16,7 +17,7 @@ DbManager::DbManager()
         qDebug() << "Database: connection ok";
     }
 }
-
+//gets scientist and his information from database and reads into Scientist vector
 vector<Scientist> DbManager::getScientists()
 {
     vector<Scientist> scientists;
@@ -41,7 +42,7 @@ vector<Scientist> DbManager::getScientists()
     }
     return scientists;
 }
-
+//gets computer and his information from database and reads into Computer vector
 vector<Computer> DbManager::getComputers(){
 
     vector<Computer> computers;
@@ -69,3 +70,110 @@ vector<Computer> DbManager::getComputers(){
 
     return computers;
 }
+//checks if scientist already exist in the database
+bool DbManager::scientistExists(const string& searchData) const
+{
+    bool exists = false;
+
+    QSqlQuery checkQuery;
+    checkQuery.prepare("SELECT Name FROM Scientists WHERE (Name) VALUES (:Name)");
+    checkQuery.bindValue(":Name",QString::fromStdString(searchData));
+
+    if (checkQuery.exec())
+    {
+        if (checkQuery.next())
+        {
+            exists = true;
+        }
+    }
+    else
+    {
+        qDebug() << "Error in scientist exists: " << checkQuery.lastError();
+    }
+
+    return exists;
+}
+//checks if computer already exist in the database
+bool DbManager::computerExists(const string& searchData) const
+{
+    bool exists = false;
+
+    QSqlQuery checkQuery;
+    checkQuery.prepare("SELECT Name FROM Computers WHERE (Name) VALUES (:Name)");
+    checkQuery.bindValue(":Name",QString::fromStdString(searchData));
+
+    if (checkQuery.exec())
+    {
+        if (checkQuery.next())
+        {
+            exists = true;
+        }
+    }
+    else
+    {
+        qDebug() << "Error in computer exists: " << checkQuery.lastError();
+    }
+
+    return exists;
+}
+//gets the info on Scientist which is searced for
+vector<Scientist> DbManager::searchScientist(string& searchData)
+{
+    vector<Scientist> foundScientist;
+    QSqlQuery query;
+    if (scientistExists(searchData))
+    {
+
+        query.prepare("SELECT Name FROM Scientists WHERE (Name) VALUES (:Name)");
+        query.bindValue(":Name",QString::fromStdString(searchData));
+
+        string name = query.value("Name").toString().toStdString();
+        string gender = query.value("Gender").toString().toStdString();
+
+        int yearOfBirth = query.value("Birthyear").toUInt();
+        int yearOfDeath = query.value("Deathyear").toUInt();
+
+        Scientist scientist(name, gender, yearOfBirth, yearOfDeath);
+
+        foundScientist.push_back(scientist);
+    }
+    else
+    {
+        qDebug() << "This scientist does not exist in this database " << query.lastError();
+    }
+
+    return foundScientist;
+}
+//gets the info on Computer which is searched for
+vector<Computer> DbManager::searchComputer(string& searchData)
+{
+    vector<Computer> foundComputer;
+    QSqlQuery query;
+    if(computerExists(searchData))
+    {
+
+        query.prepare("SELECT Name FROM Computers WHERE (Name) VALUES (:Name)");
+        query.bindValue(":Name",QString::fromStdString(searchData));
+
+        string name = query.value("Name").toString().toStdString();
+
+        int Yearbuilt = query.value("Yearbuilt").toUInt();
+
+        string type = query.value("Type").toString().toStdString();
+
+        bool built = query.value("Built").toBool();
+
+       Computer computer(name, Yearbuilt, type, built);
+
+       foundComputer.push_back(computer);
+
+    }
+    else
+    {
+        qDebug() << "This computer does not exist in this database " << query.lastError();
+    }
+
+    return foundComputer;
+}
+
+
--- dbmanager.h
+++ dbmanager.h
@@ -18,6 +18,10 @@ public:
 
     vector<Scientist> getScientists();
     vector<Computer> getComputers();
+    bool scientistExists(const string& searchData) const;
+    bool computerExists(const string& searchData) const;
+    vector<Scientist> searchScientist(string& searchData);
+    vector<Computer> searchComputer(string& searchData);
 
 private:
 
