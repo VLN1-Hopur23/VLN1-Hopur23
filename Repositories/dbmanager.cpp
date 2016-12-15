4626a29c8f8eb5c2274a7335b96dd36ea68263a4
rebekka123 <rebekkagudmunds@gmail.com>
2016-12-15 15:20:10 +0000

    Christmas cleanup in dbmanager

--- Repositories/dbmanager.cpp
+++ Repositories/dbmanager.cpp
@@ -92,6 +92,7 @@ vector<Computer> DbManager::getComputers()
     return computers;
 }
 
+// adding to database
 bool DbManager::addScientist(const Scientist& scientist, int& id)
 {
     if (!_db.isOpen())
@@ -110,6 +111,82 @@ bool DbManager::addScientist(const Scientist& scientist, int& id)
     if(queryAdd.exec())
     {
         id = queryAdd.lastInsertId().toInt();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool DbManager::addComputer(const Computer& computer, int& id)
+{
+    if (!_db.isOpen())
+    {
+        _db.open();
+    }
+
+    QSqlQuery queryAdd(_db);
+
+    queryAdd.prepare("INSERT INTO computers (Name, Yearbuilt, Type, Built) VALUES (:Name, :Yearbuilt, :Type, :Built)");
+    queryAdd.bindValue(":Name", QString::fromStdString(computer.getName()));
+    queryAdd.bindValue(":Yearbuilt", computer.getYearBuilt());
+    queryAdd.bindValue(":Type", QString::fromStdString(computer.getType()));
+    queryAdd.bindValue(":Built", computer.getBuilt());
+
+    if(queryAdd.exec())
+    {
+        id = queryAdd.lastInsertId().toInt();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool DbManager::addIntersect(const int& scientistID, const int& computerID)
+{
+    if (!_db.isOpen())
+    {
+        _db.open();
+    }
+
+    QSqlQuery queryAdd(_db);
+    queryAdd.prepare("INSERT INTO Computers_Scientists (ComputerID,ScientistID) VALUES (:computerID, :scientistID)");
+    queryAdd.bindValue(":computerID", computerID);
+    queryAdd.bindValue(":scientistID", scientistID);
+
+    if(queryAdd.exec())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// Deletes computer with chosen ID number from database and his previous connection to scientist
+bool DbManager::deleteScientist(const int ID)
+{
+    if (!_db.isOpen())
+    {
+        _db.open();
+    }
+
+    QSqlQuery queryDelete(_db);
+    queryDelete.prepare("DELETE FROM Scientists WHERE ScientistID = (:ScientistID)");
+    queryDelete.bindValue(":ScientistID",ID);
+    queryDelete.exec();
+
+    QSqlQuery queryDeleteConnection(_db);
+    queryDeleteConnection.prepare("DELETE FROM Computers_Scientists WHERE ScientistID = (:ScientistID)");
+    queryDeleteConnection.bindValue(":ScientistID",ID);
+    queryDeleteConnection.exec();
+
+    if(queryDelete.exec())
+    {
         return true;
     }
     else
@@ -153,60 +230,6 @@ void DbManager::deleteConnection(const int ID)
     queryDeleteConnection.prepare("DELETE FROM Computers_Scientists WHERE Computer_ScientistID = (:Computer_ScientistID)");
     queryDeleteConnection.bindValue(":Computer_ScientistID",ID);
     queryDeleteConnection.exec();
-}
-
-// Deletes computer with chosen ID number from database and his previous connection to scientist
-bool DbManager::deleteScientist(const int ID)
-{
-    if (!_db.isOpen())
-    {
-        _db.open();
-    }
-
-    QSqlQuery queryDelete(_db);
-    queryDelete.prepare("DELETE FROM Scientists WHERE ScientistID = (:ScientistID)");
-    queryDelete.bindValue(":ScientistID",ID);
-    queryDelete.exec();
-
-    QSqlQuery queryDeleteConnection(_db);
-    queryDeleteConnection.prepare("DELETE FROM Computers_Scientists WHERE ScientistID = (:ScientistID)");
-    queryDeleteConnection.bindValue(":ScientistID",ID);
-    queryDeleteConnection.exec();
-
-    if(queryDelete.exec())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool DbManager::addComputer(const Computer& computer, int& id)
-{
-    if (!_db.isOpen())
-    {
-        _db.open();
-    }
-
-    QSqlQuery queryAdd(_db);
-
-    queryAdd.prepare("INSERT INTO computers (Name, Yearbuilt, Type, Built) VALUES (:Name, :Yearbuilt, :Type, :Built)");
-    queryAdd.bindValue(":Name", QString::fromStdString(computer.getName()));
-    queryAdd.bindValue(":Yearbuilt", computer.getYearBuilt());
-    queryAdd.bindValue(":Type", QString::fromStdString(computer.getType()));
-    queryAdd.bindValue(":Built", computer.getBuilt());
-
-    if(queryAdd.exec())
-    {
-        id = queryAdd.lastInsertId().toInt();
-        return true;
-    }
-    else
-    {
-        return false;
-    }
 }
 
 // Returns vector with all computers associated with the scientist/s
@@ -318,6 +341,72 @@ vector<Scientist> DbManager::searchScientist(const string& searchData)
     return foundScientists;
 }
 
+// Gets the info on Computer which is searched for
+vector<Computer> DbManager::searchComputer(string& searchData)
+{
+    if (!_db.isOpen())
+    {
+        _db.open();
+    }
+
+    vector<Computer> foundComputer;
+    QSqlQuery query(_db);
+
+    if (isdigit(searchData.at(0)))
+    {
+        query.exec("SELECT * FROM Computers WHERE (Yearbuilt) LIKE '%" + QString::fromStdString(searchData) + "%'");
+    }
+    else
+    {
+        query.exec("SELECT * FROM Computers WHERE (Name || Type) LIKE '%" + QString::fromStdString(searchData) + "%'");
+    }
+
+    while(query.next())
+    {
+        int computerID = query.value("ComputerID").toUInt();
+        string name = query.value("Name").toString().toStdString();
+        int yearBuilt = query.value("Yearbuilt").toUInt();
+        string type = query.value("Type").toString().toStdString();
+        bool built = query.value("Built").toBool();
+
+        Computer computer(computerID, name, yearBuilt, type, built);
+
+        foundComputer.push_back(computer);
+    }
+    return foundComputer;
+}
+
+vector<Computer> DbManager::searchComputerPeriod(int yearFrom, int yearTo)
+{
+   if (!_db.isOpen())
+   {
+    _db.open();
+   }
+
+   QSqlQuery query(_db);
+   vector<Computer> foundComputer;
+
+   query.prepare("SELECT * FROM Computers WHERE Yearbuilt BETWEEN (:YearFrom) AND (:YearTo)");
+   query.bindValue(":YearFrom", yearFrom);
+   query.bindValue(":YearTo", yearTo);
+
+   query.exec();
+
+   while(query.next())
+   {
+       int computerID = query.value("ComputerID").toUInt();
+       string name = query.value("Name").toString().toStdString();
+       int yearBuilt = query.value("Yearbuilt").toUInt();
+       string type = query.value("Type").toString().toStdString();
+       bool built = query.value("Built").toBool();
+
+       Computer computer(computerID, name, yearBuilt, type, built);
+
+       foundComputer.push_back(computer);
+   }
+   return foundComputer;
+}
+
 vector<Computer> DbManager::filterComputer(const string& Command, const string& searchData)
 {
     if (!_db.isOpen())
@@ -378,41 +467,7 @@ vector<Scientist> DbManager::filterScientist(const string& Command, const string
     return foundScientists;
 }
 
-// Gets the info on Computer which is searched for
-vector<Computer> DbManager::searchComputer(string& searchData)
-{
-    if (!_db.isOpen())
-    {
-        _db.open();
-    }
-
-    vector<Computer> foundComputer;
-    QSqlQuery query(_db);
-
-    if (isdigit(searchData.at(0)))
-    {
-        query.exec("SELECT * FROM Computers WHERE (Yearbuilt) LIKE '%" + QString::fromStdString(searchData) + "%'");
-    }
-    else
-    {
-        query.exec("SELECT * FROM Computers WHERE (Name || Type) LIKE '%" + QString::fromStdString(searchData) + "%'");
-    }
-
-    while(query.next())
-    {
-        int computerID = query.value("ComputerID").toUInt();
-        string name = query.value("Name").toString().toStdString();
-        int yearBuilt = query.value("Yearbuilt").toUInt();
-        string type = query.value("Type").toString().toStdString();
-        bool built = query.value("Built").toBool();
-
-        Computer computer(computerID, name, yearBuilt, type, built);
-
-        foundComputer.push_back(computer);
-    }
-    return foundComputer;
-}
-
+// Edit section
 string DbManager::editScientistName(const int& id, const string& newName)
 {
     if (!_db.isOpen())
@@ -627,58 +682,4 @@ string DbManager::stringToLower(string str)
     }
     return result;
 }
-
-bool DbManager::addIntersect(const int& scientistID, const int& computerID)
-{
-    if (!_db.isOpen())
-    {
-        _db.open();
-    }
-
-    QSqlQuery queryAdd(_db);
-    queryAdd.prepare("INSERT INTO Computers_Scientists (ComputerID,ScientistID) VALUES (:computerID, :scientistID)");
-    queryAdd.bindValue(":computerID", computerID);
-    queryAdd.bindValue(":scientistID", scientistID);
-
-    if(queryAdd.exec())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
- vector<Computer> DbManager::searchComputerPeriod(int yearFrom, int yearTo)
-{
-    if (!_db.isOpen())
-    {
-     _db.open();
-    }
-
-    QSqlQuery query(_db);
-    vector<Computer> foundComputer;
-
-    query.prepare("SELECT * FROM Computers WHERE Yearbuilt BETWEEN (:YearFrom) AND (:YearTo)");
-    query.bindValue(":YearFrom", yearFrom);
-    query.bindValue(":YearTo", yearTo);
-
-    query.exec();
-
-    while(query.next())
-    {
-        int computerID = query.value("ComputerID").toUInt();
-        string name = query.value("Name").toString().toStdString();
-        int yearBuilt = query.value("Yearbuilt").toUInt();
-        string type = query.value("Type").toString().toStdString();
-        bool built = query.value("Built").toBool();
-
-        Computer computer(computerID, name, yearBuilt, type, built);
-
-        foundComputer.push_back(computer);
-    }
-    return foundComputer;
-}
-
 
